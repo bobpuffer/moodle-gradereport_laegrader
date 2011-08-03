e871ea9f12302c898847f92227217e3c58af7868
Daniel Landau <dlandau@reed.edu>
2011-08-03 13:20:56 -0700

    adding remaining TinyMCE customizations

--- lib/editor/tinymce/jscripts/tiny_mce/tiny_mce_gzip.php
+++ lib/editor/tinymce/jscripts/tiny_mce/tiny_mce_gzip.php
@@ -1,8 +1,8 @@
 <?php // $Id$
 /**
- * $RCSfile$
- * $Revision$
- * $Date$
+ * $RCSfile: tiny_mce_gzip.php,v $
+ * $Revision: 1.1 $
+ * $Date: 2006/03/04 15:57:32 $
  *
  * @version 1.07
  * @author Moxiecode
