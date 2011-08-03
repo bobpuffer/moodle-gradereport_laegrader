91687b25dc1283e582c64cd471def7b3d321da08
Daniel Landau <nprolog@postalmodern.com>
2011-08-03 12:13:17 -0700

    adding Quickmail block

---
+++ blocks/quickmail/tabs.php
@@ -0,0 +1,28 @@
+<?php // $Id$
+/**
+ * Tabs for Quickmail
+ *
+ * @author Mark Nielsen
+ * @version $Id$
+ * @package quickmail
+ **/
+
+    if (empty($course)) {
+        error('Programmer error: cannot call this script without $course set');
+    }
+    if (!isset($instanceid)) {
+        $instanceid = 0;
+    }
+    if (empty($currenttab)) {
+        $currenttab = 'compose';
+    }
+
+    $rows = array();
+    $row = array();
+
+    $row[] = new tabobject('compose', "$CFG->wwwroot/blocks/quickmail/email.php?id=$course->id&amp;instanceid=$instanceid", get_string('compose', 'block_quickmail'));
+    $row[] = new tabobject('history', "$CFG->wwwroot/blocks/quickmail/emaillog.php?id=$course->id&amp;instanceid=$instanceid", get_string('history', 'block_quickmail'));
+    $rows[] = $row;
+
+    print_tabs($rows, $currenttab);
+?>
