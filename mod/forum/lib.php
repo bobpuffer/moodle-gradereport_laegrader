de3f78549c721ba70d1931a06f0702b3a935fcaf
Charles Fulton <mackensen@gmail.com>
2012-01-12 11:29:25 -0800

    CLAMP-246 add anonymous forum support

--- mod/forum/lib.php
+++ mod/forum/lib.php
@@ -49,6 +49,10 @@ if (!defined('FORUM_CRON_USER_CACHE')) {
     define('FORUM_CRON_USER_CACHE', 5000);
 }
 
+define('FORUM_ANONYMOUS_NEVER', 0);
+define('FORUM_ANONYMOUS_ALWAYS', 1);
+define('FORUM_ANONYMOUS_ALLOWED', 2);
+
 /// STANDARD FUNCTIONS ///////////////////////////////////////////////////////////
 
 /**
@@ -3169,6 +3173,11 @@ function forum_print_post($post, $discussion, $forum, &$cm, $course, $ownpost=fa
     // String cache
     static $str;
 
+    // If anonymous is the poster check who the actual owner is
+    if(!empty($post->hiddenuserid) && !empty($USER)) {
+        $ownpost = ($USER->id == $post->hiddenuserid);
+    }
+
     $modcontext = get_context_instance(CONTEXT_MODULE, $cm->id);
 
     $post->course = $course->id;
@@ -4260,6 +4269,10 @@ function forum_add_new_post($post, $mform, &$message) {
     $post->mailed     = "0";
     $post->userid     = $USER->id;
     $post->attachment = "";
+    
+    if($post->anonymous || ($forum->anonymous == FORUM_ANONYMOUS_ALWAYS)) {
+        $post = forum_scrub_userid($post);
+    }
 
     $post->id = $DB->insert_record("forum_posts", $post);
     $post->message = file_save_draft_area_files($post->itemid, $context->id, 'mod_forum', 'post', $post->id, array('subdirs'=>true), $post->message);
@@ -4366,6 +4379,10 @@ function forum_add_discussion($discussion, $mform=null, &$message=null, $userid=
     $post->course        = $forum->course; // speedup
     $post->mailnow       = $discussion->mailnow;
 
+    if($discussion->anonymous || ($forum->anonymous == FORUM_ANONYMOUS_ALWAYS)) {
+        $post = forum_scrub_userid($post);
+    }
+
     $post->id = $DB->insert_record("forum_posts", $post);
 
     // TODO: Fix the calling code so that there always is a $cm when this function is called
@@ -4380,7 +4397,7 @@ function forum_add_discussion($discussion, $mform=null, &$message=null, $userid=
     $discussion->firstpost    = $post->id;
     $discussion->timemodified = $timenow;
     $discussion->usermodified = $post->userid;
-    $discussion->userid       = $userid;
+    $discussion->userid       = $post->userid;
 
     $post->discussion = $DB->insert_record("forum_discussions", $discussion);
 
@@ -8270,4 +8287,37 @@ function forum_get_posts_by_user($user, array $courses, $musthaveaccess = false,
     }
 
     return $return;
+}
+
+/**
+ * Add the anonymous user if necessary
+ */
+function forum_add_anonymous_user() {
+    global $CFG, $DB;
+    if(empty($CFG->anonymous_userid)) {
+        $anon_user = new stdClass;
+        $anon_user->username = 'anonymous_user';
+        $anon_user->password = hash_internal_user_password(mt_rand());
+        $anon_user->auth = 'nologin';
+        $anon_user->firstname = 'Anonymous';
+        $anon_user->lastname = 'User';
+        if($result = $DB->insert_record('user', $anon_user)) {
+            set_config('anonymous_userid', $result);
+        } else {
+            print_error("Failed to create anonymous user");
+            return false;
+        }
+    }
+    return true;
+}
+
+/**
+ * Anonymizes the forum userid (if necessary)
+ */
+function forum_scrub_userid($post) {
+    global $CFG;
+
+    $post->hiddenuserid = $post->userid;
+    $post->userid = $CFG->anonymous_userid;    
+    return $post;
 }
