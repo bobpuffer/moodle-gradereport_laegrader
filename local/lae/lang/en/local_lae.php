19a5821886aca6d77b5b9d3a66d9dea049ca6938
Charles Fulton <mackensen@gmail.com>
2013-06-10 08:22:45 -0700

    CLAMP-501 Import Local LAE plugin

---
+++ local/lae/lang/en/local_lae.php
@@ -0,0 +1,20 @@
+<?php
+$string['auser_firstname'] = 'Anonymous';
+$string['auser_lastname'] = 'User';
+$string['forum:allowanonymous'] = 'Anonymize posts?';
+$string['forum:allowanonymous_help'] = 'If anonymous posts for forums are enabled, users can post messages in forums and discussions without revealing their name to other users. All such posts will appear as by an anonymous user.
+
+The instructor can choose three different modes for this setting. Note that the logs still report user activity so this is not truly anonymous. 
+
+There are three choices for this setting:
+
+* Never [default]: participants may not post anonymously.
+* Always: all posts are anonymous.
+* Sometimes: participants may choose to post anonymously.';
+$string['forum:anonno'] = 'No, never';
+$string['forum:anonoptional'] = 'Yes, let the user decide';
+$string['forum:anonyes'] = 'Yes, always';
+$string['forum:anonymouspost'] = 'Post anonymously';
+$string['forum:configenableanonymouspost'] = 'Set to \'yes\' if you want to enable Anonymous Forums.';
+$string['pluginname'] = 'Liberal Arts Edition';
+?>
