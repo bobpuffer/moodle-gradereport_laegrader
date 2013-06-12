36ef25249759dc652058e6115b918c4fbfcec80a
Charles Fulton <mackensen@gmail.com>
2013-06-12 10:21:17 -0700

    Squashed 'report/roster/' content from commit 82d2786
    git-subtree-dir: report/roster
    git-subtree-split: 82d278620dc5ce3ef034ad7d9e429b2d745ef4d2

---
+++ locallib.php
@@ -0,0 +1,26 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Local library functions for the roster report.
+ *
+ * @package   report_roster
+ * @copyright 2013 Lafayette College ITS
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+define('ROSTER_MODE_DISPLAY', 'display');
+define('ROSTER_MODE_PRINT', 'print');
