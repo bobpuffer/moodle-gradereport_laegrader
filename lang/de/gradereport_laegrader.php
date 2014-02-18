fe7e4319484b8ae9ed0f3651d3b9bef12d6e06e0
Guido Roessling <guido@informatik.tu-darmstadt.de>
2014-02-18 18:14:03 +0100

    Create gradereport_laegrader.php
    German translation, "formal"

---
+++ lang/de/gradereport_laegrader.php
@@ -0,0 +1,49 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'laegradereport_grader', language 'de', branch 'MOODLE_20_STABLE'
+ *
+ * @package   laegradereport_grader
+ * @copyright 2010 onwards Bob Puffer
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['accuratetotals'] = 'Genaue Punktsumme (der LAE User Report sollte auch benutzt werden, wenn präzise Punkte angeschaltet sind, damit die Berichte übereinstimmen, verfügbar unte\
+r http://github.com/bobpuffer/laeuser.git)';
+$string['ajaxclicktoclose'] = 'Klicken Sie auf diese Box, um sie zu entfernen';
+$string['ajaxerror'] = 'Fehler';
+$string['ajaxfailedupdate'] = '[1] konnten nicht für [2] aktualisiert werden';
+$string['ajaxfieldchanged'] = 'Das aktuell bearbeitete Feld wurde verändert, wollen Sie den aktualisierten Wert benutzen?';
+$string['ajaxchoosescale'] = 'Auswählen';
+$string['changedisplay'] = 'Anzeige dieses Elements umschalten zwischen Prozent, Gleitkommazahl und Buchstabe';
+$string['clearoverrides'] = 'Alle überschriebenen Bedingungen in dieser Spalte löschen';
+$string['configaccuratetotals'] = 'Kategorien- und Kurs-Gesamtergebnis, präzise berechnet';
+$string['coursetotal'] = 'Ergebnis für den KURS ';
+$string['categorytotal'] = 'Ergebnis für die KATEGORIE ';
+$string['copytoexcel'] = 'Inhalt des Notenbuchs als CSV-Datei für Excel, Google Docs u. Ä. herunterladen';
+$string['grader:manage'] = 'Bewertungsanzeige verwalten';
+$string['laegrader_columnwidth'] = 'Anzahl Buchstaben bevor Überschriften umgebrochen werden';
+$string['laegrader_extrafields'] = 'Weitere Nutzerfelder anzeigen';
+$string['laegrader_extrafields_help'] = 'Weitere Nutzerfelder im Bewertungsbericht anzeigen (etwa Email oder ID, wie vom Administrator in den Einstellungen festgelegt)';
+$string['laegrader_reportheight'] = 'Höhe (in Pixeln) des scrollbaren Bereichs der LAE Bewertungsanzeige';
+$string['laegrader:view'] = 'LAE Bewertungsanzeige';
+$string['pluginname'] = 'LAE Bewertungsanzeige';
+$string['preferences'] = 'Einstellungen für die LAE Bewertungsanzeige';
+$string['showclearoverrides'] = 'Icons zum Löschen von überschriebenen Bedingungen in Spalten anzeigen';
+$string['showzerofill'] = 'Icons anzeigen, mit denen alle leeren Notenfelder in dieser Spalte mit dem Wert 0 versehen werden können';
+$string['zerofill'] = 'Leere Notenfelder in dieser Spalte mit dem Wert 0 versehen (Änderungen werden nicht automatisch gespeichert)';
